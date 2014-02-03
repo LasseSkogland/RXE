b1b50234e788f40c3a8428f9715b167fba87a10d
Lasse Skogland <admin@gamearc.info>
2014-02-03 13:29:20 +0100

    Various Fixes You can now set BUFFER_SIZE and read larger files, default buffer size 8196 Added Argtable 2 for better argument handling. Added Custom random generator. SORUCE: http://www.firstpr.com/dsp/rand31/

--- RXE.c
+++ RXE.c
@@ -1,84 +1,89 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <windows.h>
+#include <argtable2.h>
 
-#define debug(MSG) printf("%s\n", MSG)
+#define println(MSG) printf("%s\n", MSG)
+long seed = 0L;
+int BUFFER_SIZE = 8196;
 
-char *RXE(char *file, char *key) {
-	// Open input file
-	FILE *f;
-	f = fopen(file, "rb");
+long unsigned int RXE_rand() {
+	long unsigned int hi, lo;
+	lo = 16807 * (seed & 0xFFFF);
+	hi = 16807 * (seed >> 16);
 	
-	// Setup variables for reading
-	int c, i = 0, read = 0, fsize = 0;
-	
-	// Get size of file
-	fseek(f, 0L, SEEK_END);
-	fsize = ftell(f);
-	printf("Size: %i bytes\n", fsize);
-	rewind(f);
-	
-	// Allocate input and output buffers
-	char *data = calloc(fsize + 1, sizeof(char)), *outdata = calloc(fsize + 1, sizeof(char));
-	
-	// Read file to memory
-	read = fread(data, sizeof(char), fsize, f);
-	
-	// Close file
-	fclose(f);
-	
-	// Compare FileSize(fSize) to bytes read(read) 
-	if(fsize == read) {
-	
-		// Generate key/seed from password
-		int tkey = 0;
-		i = 0, read = 0;
-		while(i < strlen(key)){
-			tkey += (int)key[i++];
-		}
-		
-		// Seed random generator
-		srand(tkey);
-		
-		i = 0;
-		// Open file
-		f = fopen(file, "wb");
-		
-		/*
-		 * This is where the magic happens!
-		 * Algorithm works like this: (pseudo code)
-		 *
-		 * Seed = HashCode(Password)
-		 * SeedRandomGenerator(Seed)
-		 * Data[] = readFileToEnd(inputfile)
-		 * OutputData[] = AllocateMemory(inputfile.size)
-		 * for (index++) OutputData[index] = Data[index] xor GetRandomNumber()
-		 * 
-		 */
-		for(i = 0; i < fsize; i++) outdata[i] = data[i] ^ rand();
-		
-		// Write data to original file, this writes over previous data(Be careful!!!) 
-		fwrite(outdata, sizeof(char), fsize, f);
-		
-		// Close File
-		fclose(f);
-		
-	} else printf("Failed to read file...\n");
-	// Free buffers
-	free(data);
-	free(outdata);
-	
+	lo += (hi & 0x7FFF) << 16;
+	lo += hi >> 15;
+	if (lo > 0x7FFFFFF) lo -= 0x7FFFFFFF;
+	return (seed = (long)lo);
 }
 
-void main(int argc, char **argv){
-	if(argc == 3) {
-		RXE(argv[1], argv[2]);
-	}else if(argc == 2) {
-		RXE(argv[1], "default");
-	}else{
-		debug("RXE 0.3");
-		debug("Usage: ");
-		debug("rxe <INPUTFILE> (PASSWORD)");
-		debug("Password is optional, it defaults to \"default\"");
+void RXE_Crypt(char *indata, char *outdata, int size) {
+	int i = 0;
+	for(i = 0; i < size; i++) outdata[i] = indata[i] ^ RXE_rand();
+}
+
+void RXE_Init(char *key) {
+	long i = 0;
+	int klen = __builtin_strlen(key);
+	while(i < klen){
+		seed += (long)(key[i++] * key[(klen -1) - i]);
+	}
+}
+
+void main(int argc, char *argv[]){
+	struct arg_file *ifile = arg_file1("i", "input-file", "INPUT_FILE", "Set Input File");
+	struct arg_file *ofile = arg_file0("o", "output-file", "OUTPUT_FILE", "Set Output File");
+	struct arg_str *pass = arg_str0("p", "password", "PASSWORD","Set Encryption Password");
+	struct arg_int *buf = arg_int0("b", "buffer-size", "BUFFER_SIZE","Set Buffer Size");
+	struct arg_end *end = arg_end(20);
+	void *argtable[] = {ifile, ofile, pass, buf, end};
+	int nerrors = arg_parse(argc, argv, argtable);
+	
+	if (nerrors > 0) {
+		arg_print_errors(stdout, end, "rxe");
+		printf("\nUsage: rxe");
+		arg_print_syntax(stdout, argtable, "\n");
+		arg_print_glossary(stdout, argtable, "  %-10s %s\n");
+	} else {
+		if(pass->count > 0) RXE_Init((char *)pass->sval[0]);
+		else RXE_Init("DEFAULT");
+		if (buf->count > 0) {
+			BUFFER_SIZE = buf->ival[0];
+		}
+		FILE *outfile, *infile = fopen(ifile->filename[0], "rb");
+		if (ofile->count > 0) outfile = fopen(ofile->filename[0], "wb");
+		else {
+			char farr[2048];
+			__builtin_strcpy(farr, ifile->filename[0]);
+			__builtin_strcat(farr, ".rxe");
+			outfile = fopen(farr, "wb");
+		}
+		int size;
+		fseek(infile, 0L, SEEK_END);
+		size = ftell(infile);
+		rewind(infile);
+		int itmp = 0, otmp = 0;
+		char *outdata, *indata = calloc(BUFFER_SIZE, sizeof(char));
+		outdata = calloc(BUFFER_SIZE, sizeof(char));
+		while(size > 0) {
+			if (size >= BUFFER_SIZE) {
+				itmp = fread(indata, sizeof(char), BUFFER_SIZE, infile);
+				printf("R/W: %i/", itmp);
+				RXE_Crypt(indata, outdata, BUFFER_SIZE);
+				otmp = fwrite(outdata, sizeof(char), BUFFER_SIZE, outfile);
+				printf("%i\n", otmp);
+				size -= BUFFER_SIZE;
+			} else {
+				itmp = fread(indata, sizeof(char), size, infile);
+				printf("R/W: %i/", itmp);
+				RXE_Crypt(indata, outdata, size);
+				otmp = fwrite(outdata, sizeof(char), size, outfile);
+				printf("%i\n", otmp);
+				size = 0;
+			}
+			
+		}
+		fclose(outfile);
+		fclose(infile);
 	}
 }
